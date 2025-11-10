775b56253cde2d10551904a1bb3ef3deb39c8868
kimJAVAN <ookim7717@gmail.com>
2025-11-10 15:21:27 +0900

    [chore]_11/10 - gitignore / readme 추가 header footer 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Header from "@/components/Header";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
