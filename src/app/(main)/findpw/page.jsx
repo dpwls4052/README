935a7e9aa94665821f5c77ccd3a64b8592bdc584
Juhyoungd <jhapoy106@naver.com>
2025-11-14 15:28:05 +0900

    11/14 - id, pw 찾기 구현

---
+++ src/app/(main)/findpw/page.jsx
@@ -0,0 +1,19 @@
+import FindPWForm from "@/components/auth/FindPWForm";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+
+
+export default function FindPWPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-[#FEFAE0]">
+            <Image
+              src={logo}
+              alt="README Logo"
+              width={200}
+              height={200}
+              className="mb-8 object-contain"
+            />
+      <FindPWForm />
+    </div>
+  );
+}
