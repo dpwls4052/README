033f1cb9ef3dcedc9daca829a8702c48a1411b8d
rkddusen <k99endus@naver.com>
2025-11-19 11:49:45 +0900

    [fix]_11/19-도서 조회(검색) 로직 수정

--- src/service/booksService.js
+++ src/service/booksService.js
@@ -1,10 +1,20 @@
 // services/booksService.ts
 
-export async function getBooks({ page = 1, category, keyword }) {
+export async function getBooks({
+  page = 1,
+  pageSize,
+  category,
+  search,
+  orderField,
+  orderDirection,
+}) {
   const params = new URLSearchParams();
   params.append("page", page.toString());
+  if (pageSize) params.append("pageSize", pageSize);
   if (category) params.append("category", category);
-  if (keyword) params.append("keyword", keyword);
+  if (search) params.append("search", search);
+  if (orderField) params.append("orderField", orderField);
+  if (orderDirection) params.append("orderDirection", orderDirection);
 
   const res = await fetch(`/api/books?${params.toString()}`, {
     method: "GET",
