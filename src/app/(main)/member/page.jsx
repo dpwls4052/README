1216ba4e55c0a7aa8b4f465fcbf8416cb30678a4
kimJAVAN <ookim7717@gmail.com>
2025-11-28 15:18:40 +0900

    [fix]_11/28 - 마이페이지 접근차단

--- src/app/(main)/member/page.jsx
+++ src/app/(main)/member/page.jsx
@@ -1,10 +1,13 @@
 import { Suspense } from "react";
 import MemberContent from "@/components/member/MemberContent";
+import ProtectedRoute from "@/components/common/ProtectedRoute";
 
 const MyPage = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <MemberContent />
+      <ProtectedRoute>
+        <MemberContent />
+      </ProtectedRoute>
     </Suspense>
   );
 };
