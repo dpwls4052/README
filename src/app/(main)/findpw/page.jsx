c51e1b2bb39e101310b57c4d89079ef18cd68c52
rkddusen <k99endus@naver.com>
2025-11-28 15:28:23 +0900

    [fix]_11/28-로그인 유저의 비회원 전용 페이지 접속 제한  #62

--- src/app/(main)/findpw/page.jsx
+++ src/app/(main)/findpw/page.jsx
@@ -1,19 +1,21 @@
 import FindPWForm from "@/components/auth/FindPWForm";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
-
+import PublicOnlyRoute from "@/components/common/PublicOnlyRoute";
 
 export default function FindPWPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#FEFAE0]">
-            <Image
-              src={logo}
-              alt="README Logo"
-              width={200}
-              height={200}
-              className="mb-8 object-contain py-25"
-            />
-      <FindPWForm />
-    </div>
+    <PublicOnlyRoute>
+      <div className="flex flex-col items-center justify-center h-screen bg-[#FEFAE0]">
+        <Image
+          src={logo}
+          alt="README Logo"
+          width={200}
+          height={200}
+          className="object-contain mb-8 py-25"
+        />
+        <FindPWForm />
+      </div>
+    </PublicOnlyRoute>
   );
 }
--- src/app/(main)/signUp/page.jsx
+++ src/app/(main)/signUp/page.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import SignupForm from "@/components/auth/SignupForm";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
+import PublicOnlyRoute from "@/components/common/PublicOnlyRoute";
 
 const SignupPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#FFFAE5]">
-      <Image
-        src={logo}
-        alt="README Logo"
-        width={200}
-        height={200}
-        className="mb-8 object-contain py-25"
-      />
+    <PublicOnlyRoute>
+      <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#FFFAE5]">
+        <Image
+          src={logo}
+          alt="README Logo"
+          width={200}
+          height={200}
+          className="object-contain mb-8 py-25"
+        />
 
-      {/* 회원가입 폼을 감싸는 카드 */}
+        {/* 회원가입 폼을 감싸는 카드 */}
         <SignupForm />
-    </div>
+      </div>
+    </PublicOnlyRoute>
   );
 };
 
